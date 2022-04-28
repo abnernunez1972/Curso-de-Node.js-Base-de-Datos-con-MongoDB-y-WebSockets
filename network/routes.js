8309e7a51a0423733e39e9cac114307d7bda9527
abnernunez1972 <abner.nunez1972@gmail.com>
2022-04-27 21:21:31 -0500

    Session Study 27-04-2022

--- network/routes.js
+++ network/routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const message = require('../components/messages/network');
 
+// Aqui se listan las rutas de los Componentes que usara la apps
+const message = require('../components/messages/network');
+//
+
+// Aqui se agrgan las rutas al server de express 
 const routes = function (server) {
 server.use('/message', message);
 
 }
+//
 
 module.exports = routes;
